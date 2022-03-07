1e54e83017d61a6c98d110256a40264b59320652
qjawns0222 <33923212+qjawns0222@users.noreply.github.com>
2022-03-07 21:19:00 +0900

    start

---
+++ schemas/author.js
@@ -0,0 +1,32 @@
+export default {
+  name: "author",
+  title: "Author",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "role",
+      title: "role",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "image",
+    },
+  },
+};
